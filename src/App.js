e442cdf7eb2f297d4d8355caa9222ab2efa08220
mofsis <mtanios@hotmail.com>
2021-12-13 23:05:24 +0100

    opmaak niet af

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
+import Pokemon from "./components/Pokemon";
+import Logo from "./assets/png-clipart-pokemon-logo-pokemon-logo.png"
 
 function App() {
+
+  const [pokemonData, setPokemonData] = useState();
+  const [endPoint, setEndPoint] = useState("https://pokeapi.co/api/v2/pokemon/");
+
+  useEffect(() => {
+      async function fetchData() {
+          try {
+              const result = await axios.get(endPoint);
+              console.log(result.data.results);
+              setPokemonData(result.data);
+          }
+          catch (e) {
+              console.error(e);
+          }
+      }
+      fetchData();
+  }, [endPoint]);
+
   return (
-    <div>
-      Begin hier met de opdracht!
-    </div>
+      <>
+      <img src={Logo} width={500} className="logo" alt="logo"/>
+      <button
+          type="button"
+          onClick={() => setEndPoint(pokemonData.previous)}
+      >
+          Vorige
+      </button>
+      <button
+          type="button"
+          onClick={() => setEndPoint(pokemonData.next)}
+      >
+          Vorige
+      </button>
+
+          <div className="pokemon">
+          {pokemonData && <>
+
+          {pokemonData.results.map((pokemon) => {
+              return(
+              <Pokemon key={pokemon.name} endpoint={pokemon.url} />
+              )
+          }
+              )}
+          </>}
+      </div>
+      </>
   );
 }
 
